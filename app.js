67ffe9828e8d80e6e39d139ce0c896868ec2c32c
ronybarua.ethical18 <ronybarua.ethical18@gmail.com>
2021-02-14 22:25:29 +0600

    title added

--- app.js
+++ app.js
@@ -31,16 +31,11 @@ const showImages = (images) => {
     // show gallery title
     galleryHeader.style.display = 'flex';
     images.forEach(image => {
-      if (image.tags !== null) {
         let div = document.createElement('div');
         div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
         div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
         gallery.appendChild(div)
         toggleSpinner();
-      }
-      else {
-        showErrorMessage('Items not found');
-      }
     })
 
   }
